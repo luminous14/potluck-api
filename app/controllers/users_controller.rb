af9248d466459ed0736b45b5897fc211c2e86017
Ian Agne <ian.d.agne@gmail.com>
2016-01-27 15:24:05 -0600

    add ability to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
   # POST /users
   def create
     @user = User.new(user_params)
@@ -24,20 +12,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -46,6 +20,7 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :api_key)
+      pp params
+      params.require(:user).permit(:email)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :items
+
+  validates :email, uniqueness: true
+
+  before_create :generate_api_key
+
+  def generate_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
 end
